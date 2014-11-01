633627d59baa4e77dc0023b99ba6deacc0d8abf8
Chris Pressey <cpressey@catseye.tc>
2014-11-01 11:48:55 +0000

    Import of join-by-pivot experiment.

---
+++ join-at-pivot/README.md
@@ -0,0 +1,36 @@
+join-at-pivot
+=============
+
+Requirements
+------------
+
+*   Python 2.7.6 (probably works with older versions too)
+*   The `gutenberg.py` modules from [gutenizer](https://github.com/okfn/gutenizer/)
+*   A bunch of Project Gutenberg texts in plain text format
+
+Basic Strategy
+--------------
+
+*   Find the middle word ("pivot") of each sentence and split the sentence
+    into a "beginner" which ends with the pivot and an "ender" which begins
+    with the pivot.
+*   Construct a table of reasonably frequently-occuring pivots, their
+    possible beginners, and their possible enders.
+*   To make a sentence, pick a random pivot, and random beginner for that
+    pivot, and a random ender for that pivot.
+
+Sample Output
+-------------
+
+> A factory usually does not have more enterprising than the rest, had set
+> off upon an expedition. With intent, I have rushed you from the chambers
+> of Professor Jenner Monde to that closing episode at the deserted cottage;
+> I have made easy; and lo! I don't wonder. We had ascertained from the lady
+> that she went down upon the Monday by the matter, sir? Possibly an old top,
+> I said. There was two little kiddies in the Carter family whom I had loved
+> and who had thought there was no one on Earth like Uncle Jack; I could see
+> if you came across the courtyard, and so could effect an escape. Holmes
+> held up the paper so that misery; nevertheless, there was a resemblance.
+> And this is the story, if the long porter has told me rattle the dry bones
+> of the Solomons. I don't apprehend that his excellency will break it and
+> cause a large loss of fat. Yes.
---
+++ join-at-pivot/join-at-pivot.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import random
+import re
+import sys
+
+from gutenberg import GutenbergCleaner
+
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(x):
+        return x
+
+
+DEBUG = False
+
+
+def sentencify(words):
+    return ' '.join(words)
+
+
+def main(argv):
+    filenames = argv[1:]
+
+    words = []
+
+    for filename in tqdm(filenames):
+        with open(filename, 'r') as f:
+            c = GutenbergCleaner(f)
+            lines = c.extract_text().split('\n')
+            for line in lines:
+                bits = line.split()
+                for bit in bits:
+                    words.extend(bit.split('--'))
+
+    sentences = []
+    sentence = []
+    for word in tqdm(words):
+        if word.startswith(('"', "'")):
+            word = word[1:]
+        if word.endswith(('"', "'")):
+            word = word[:-1]
+        sentence.append(word)
+        if word not in ('Mr.', 'Mrs.', 'Dr.') and word.endswith(('.', '!', '?')):
+            sentences.append(sentence)
+            sentence = []
+
+    sentences.append(sentence)
+
+    beginners = {}
+    enders = {}
+
+    for sentence in tqdm(sentences):
+        l = len(sentence)
+        if l % 2 == 1:
+            # odd. odd is good
+            i = l / 2
+            middle = sentence[i]
+        elif l != 0:
+            # even. we can work with even, yes we can.
+            i = l / 2
+            middle = sentence[i]  # and i - 1
+        else:
+            continue
+        if not middle:
+            continue
+        begin = sentence[:i]
+        end = sentence[i+1:]
+        beginners.setdefault(middle, []).append(begin)
+        enders.setdefault(middle, []).append(end)
+
+    frequency = {}
+    common_keys = []
+    MIN = 100
+    for middle in tqdm(beginners):
+        if middle in enders:
+            frequency.setdefault(len(beginners[middle]) + len(enders[middle]), []).append(middle)
+            if len(beginners[middle]) < MIN and len(enders[middle]) < MIN:
+                pass
+            else:
+                common_keys.append(middle)
+
+    if DEBUG:
+        for freq in sorted(frequency, reverse=True):
+            print freq, frequency[freq]
+
+    for x in xrange(0, 10):
+        for y in xrange(0, 10):
+            middle = random.choice(common_keys)
+            beginner = random.choice(beginners[middle])
+            ender = random.choice(enders[middle])
+            if DEBUG:
+                print repr(middle), len(beginners[middle]), len(enders[middle])
+            print sentencify(beginner + [middle] + ender),
+        print
+        print
+
+    if DEBUG:
+        for middle in common_keys:
+            print '======='
+            print repr(middle)
+            print '----'
+            for x in beginners[middle]:
+                print sentencify(x)
+            print '----'
+            for x in enders[middle]:
+                print sentencify(x)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
