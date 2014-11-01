eb712d21953c9228bd5b24d3239692d153546ace
Chris Pressey <cpressey@catseye.tc>
2014-11-01 15:23:55 +0000

    Add a list of containers that can be loaded for infix-neologisms.

--- infix-neologisms/README.md
+++ infix-neologisms/README.md
@@ -15,3 +15,11 @@ Basic Strategy
     the middle.
 *   Possibly repeat step #2.
 *   Output the word and repeat from step #1.
+
+Sample Output
+-------------
+
+Running it on `../generic-corpora/containers.txt` which I just threw together
+after a few internet searches, you might get
+
+> pancupetil flacupgon bocrpailatewl chnetest poenvecuplopeuch barvialrel demiflaflagongonjohn contflavialgonainer jcarbasketboyar bucampampoulehoraket contpackchubageainer nbopackagexet bcontainerox gobasketurd envebasampouleketlope chcacoffernest nbotbagtleet bchejugstox bamphjugoraox bucbucnetketket
--- infix-neologisms/infix-neologisms.py
+++ infix-neologisms/infix-neologisms.py
@@ -4,31 +4,28 @@ import random
 import sys
 
 
-# TODO: read from file
-WORDS = [
-    'giraffe',
-    'confusion',
-    'cake',
-    'smile',
-    'horror',
-    'pizza',
-]
-
-
 def splitword(word):
     pos = len(word) / 2
     return (word[:pos], word[pos:])
 
 
 def main(argv):
-    for g in xrange(0, 100):
-        word = random.choice(WORDS)
+    words = set()
+    for filename in argv[1:]:
+        with open(filename) as f:
+            for line in f:
+                words.add(line.strip())
+
+    words = list(words)  # because random.choice doesn't work on sets
+    for g in xrange(0, 20):
+        word = random.choice(words)
         parts = splitword(word)
-        word = parts[0] + random.choice(WORDS) + parts[1]
+        word = parts[0] + random.choice(words) + parts[1]
         if random.randint(0, 1) == 0:
             parts = splitword(word)
-            word = parts[0] + random.choice(WORDS) + parts[1]
+            word = parts[0] + random.choice(words) + parts[1]
         sys.stdout.write(word + ' ')
+    sys.stdout.write("\n")
 
 
 if __name__ == '__main__':
