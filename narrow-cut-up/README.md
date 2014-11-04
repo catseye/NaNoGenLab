f6129827e0cddf5a14ea0cdcc30108af6e795794
Chris Pressey <cpressey@catseye.tc>
2014-11-04 19:29:52 +0000

    Import of narrow-cut-up experiment.

---
+++ narrow-cut-up/README.md
@@ -0,0 +1,27 @@
+narrow-cut-up
+=============
+
+Requirements
+------------
+
+*   Python 2.7.6 (probably works with older versions too)
+*   ImageMagick
+*   [Pillow](http://python-pillow.github.io/) (it might work with PIL too)
+*   Some scanned images of newspapers, books, etc.
+
+Basic Strategy
+--------------
+
+*   Like [naive-cut-up](../naive-cut-up), except that it:
+    *   scales each image to the size of the base ("canvas") image
+    *   cuts and pastes in narrow strips instead of 1/9-page chunks
+
+Usage
+-----
+
+We can use images fetched by naive-cut-up.
+
+    $ ./narrow-cut-up.py ../naive-cut-up/ca_5.jp2.png ../naive-cut-up/pages/*
+
+The result has promise, but looks like it might work best with pages
+which contain more text than images.  Screenshot forthcoming.
---
+++ narrow-cut-up/narrow-cut-up.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import random
+import sys
+
+from PIL import Image
+
+
+def main(argv):
+    base_filename = argv[1]
+    cutup_filenames = argv[2:]
+    
+    base_image = Image.open(base_filename)
+    base_width = base_image.size[0]
+    base_height = base_image.size[1]
+    print base_image
+
+    images = [base_image]
+    for filename in cutup_filenames:
+        image = Image.open(filename)
+        # hope the aspect ratio is similar...
+        image = image.resize((base_width, base_height))
+        print image
+        images.append(image)
+
+    for n in xrange(0, 5000):
+        image = random.choice(images)
+
+        crop_width = int(base_width / (2 + (random.random() * 3)))
+        crop_height = int(base_height / 80.0)
+
+        # note that for crop boxes, the 3rd and 4th elements are
+        # *positions*, not *dimensions*!
+        crop_x = random.randint(0, base_width - crop_width)
+        crop_y = random.randint(0, base_height - crop_height)
+        crop_box = (
+            crop_x, crop_y, crop_x + crop_width, crop_y + crop_height
+        )
+        crop_region = image.crop(crop_box)
+        print image, crop_box, crop_region
+    
+        paste_point = (random.randint(0, base_width - crop_width),
+                       random.randint(0, base_height - crop_height))
+        
+        base_image.paste(crop_region, paste_point)
+    
+    print "Writing output.png..."
+    base_image.save("output.png")
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
