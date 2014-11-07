07ef1d27080fcb7356d305104756ca0e28eacb13
Chris Pressey <cpressey@catseye.tc>
2014-11-07 09:27:32 +0000

    Import of ending-concordance experiment.

---
+++ ending-concordance/README.md
@@ -0,0 +1,42 @@
+ending-concordance
+==================
+
+Requirements
+------------
+
+*   Python 2.7.6 (probably works with older versions too)
+*   The `gutenberg.py` module from [gutenizer](https://github.com/okfn/gutenizer/)
+*   A bunch of Project Gutenberg texts in plain text format
+
+Basic Strategy
+--------------
+
+*   Read in all the words.
+*   Index the words based on the final two letters in each word that is four or
+    more letters long.
+*   Write out words randomly chosen from two alternating end-two-letters groups.
+
+Sample Output
+-------------
+
+When run on _Principles of Scientific Management_, I got:
+
+> management -which incompetent watch subsequent research statement stop-watch impudent stop-watch achievement Inasmuch point branch development -which excellent winch intelligent switch !
+> 
+> architect therefore object expenditure respect Therefore direct culture abstract you're product warfare architect before imperfect secure contract expenditure architect heretofore !
+> 
+> truth logic herewith public Fifth Scientific eighth drastic youth socialistic one.-fourth Scientific month drastic eighth optimistic strength optimistic health mechanic !
+> 
+> England forward demand third 21-pound afford 18-pound record around backward England board depend guard sound backward around disregard reground afford !
+> 
+> overstraining hoped falling stirred straightening traced wearing seemed directing trained interesting agreed coming completed moving educated shortening studied blundering considered !
+> 
+> capable over-inspectors timetable factors angle ancestors angle Employers double readers angle wears obstacle makers available owners double inspectors settle makers !
+> 
+> survival concise impartial house chemical advise unusual false especial because metal worse impartial increase commercial crease differential close mathematical cause !
+> 
+> severe picking future saving aware working There obtaining figure sending failure lagging nature falling there failing share machining you're loafing !
+> 
+> agents implement permits excellent accounts self-evident weights meant consists employment Experiments spent agents sentiment students inefficient servants experiment costs beneficent !
+> 
+> presents looks joints picks experiments clerks represents walks exerts books accidents books machinists lacks accounts blanks benefits risks tests clerks !
---
+++ ending-concordance/ending-concordance.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+import random
+import re
+import sys
+
+from gutenberg import GutenbergCleaner
+
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(x):
+        return x
+
+
+DEBUG = False
+
+
+def main(argv):
+    filenames = argv[1:]
+
+    words = []
+
+    for filename in tqdm(filenames):
+        with open(filename, 'r') as f:
+            c = GutenbergCleaner(f)
+            lines = c.extract_text().split('\n')
+            for line in lines:
+                bits = line.split()
+                for bit in bits:
+                    words.extend(bit.split('--'))
+
+    index = {}
+    for word in words:
+        orig_word = word
+        if word.endswith(('.', '!', '?', ';', ',')):
+            word = word[:-1]
+        if word.startswith(('"', "'", '(')):
+            word = word[1:]
+        if word.endswith(('"', "'", ')')):
+            word = word[:-1]
+        if word.endswith(('.', '!', '?', ';', ',')):
+            word = word[:-1]
+        #sys.stdout.write(word + ' ')
+        if len(word) > 4:
+            index.setdefault(word[-2:], set()).add(word)
+
+    pick_from = []
+    for key, s in index.iteritems():
+        if len(s) >= 15:  # say
+            pick_from.append(key)
+
+    for i in xrange(0, 10):
+        pick1 = list(index[random.choice(pick_from)])
+        pick2 = list(index[random.choice(pick_from)])
+        for j in xrange(0, 10):
+            sys.stdout.write(random.choice(pick1) + ' ')
+            sys.stdout.write(random.choice(pick2) + ' ')
+        sys.stdout.write('!\n\n')
+
+    sys.exit(0)
+    sentences.append(sentence)
+
+    beginners = {}
+    enders = {}
+
+    for sentence in tqdm(sentences):
+        l = len(sentence)
+        if l % 2 == 1:
+            # odd. odd is good
+            i = l / 2
+            middle = sentence[i]
+        elif l != 0:
+            # even. we can work with even, yes we can.
+            i = l / 2
+            middle = sentence[i]  # and i - 1
+        else:
+            continue
+        if not middle:
+            continue
+        begin = sentence[:i]
+        end = sentence[i+1:]
+        beginners.setdefault(middle, []).append(begin)
+        enders.setdefault(middle, []).append(end)
+
+    frequency = {}
+    common_keys = []
+    MIN = 100
+    for middle in tqdm(beginners):
+        if middle in enders:
+            frequency.setdefault(len(beginners[middle]) + len(enders[middle]), []).append(middle)
+            if len(beginners[middle]) < MIN and len(enders[middle]) < MIN:
+                pass
+            else:
+                common_keys.append(middle)
+
+    if DEBUG:
+        for freq in sorted(frequency, reverse=True):
+            print freq, frequency[freq]
+
+    for x in xrange(0, 10):
+        for y in xrange(0, 10):
+            middle = random.choice(common_keys)
+            beginner = random.choice(beginners[middle])
+            ender = random.choice(enders[middle])
+            if DEBUG:
+                print repr(middle), len(beginners[middle]), len(enders[middle])
+            print sentencify(beginner + [middle] + ender),
+        print
+        print
+
+    if DEBUG:
+        for middle in common_keys:
+            print '======='
+            print repr(middle)
+            print '----'
+            for x in beginners[middle]:
+                print sentencify(x)
+            print '----'
+            for x in enders[middle]:
+                print sentencify(x)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
