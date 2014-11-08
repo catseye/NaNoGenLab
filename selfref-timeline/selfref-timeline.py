dbaeae6a046e3afff2a4c02bcc317da86a8da920
Chris Pressey <cpressey@catseye.tc>
2014-11-08 15:17:48 +0000

    Almost there!  Just need to reduce 'that time when this moment'

--- selfref-timeline/selfref-timeline.py
+++ selfref-timeline/selfref-timeline.py
@@ -5,29 +5,36 @@ import sys
 
 
 class Event(object):
-    def __init__(self):
-        self.target_event = None
+    pass
 
 
 class Sighting(Event):
     def __init__(self):
         self.sight = random.choice(
-            ('UFO', 'bigfoot', 'ghost', 'black helicopter')
+            ('UFO', 'bigfoot', 'ghost', 'black helicopter', 'chupacabra',
+             'Yeti', 'Loch Ness monster')
         ) + ' ' + random.choice(
             ('on vacation',
              'just sitting there',
              'cheating at cards',
-             'ranting obnoxiously'
+             'ranting obnoxiously',
+             'trimming its nails',
+             'looking pensive',
+             'hovering overhead',
             )
         )
-        self.target_event = self  # too-clever way of defeating filter
 
 
-class Prediction(Event):
+class ReferentialEvent(Event):
+    def __init__(self):
+        self.target_event = None
+
+
+class Prediction(ReferentialEvent):
     pass
 
 
-class Memory(Event):
+class Memory(ReferentialEvent):
     pass
 
 
@@ -35,17 +42,27 @@ def render_toplevel(event):
     starting_at = event
     mentioned = set([starting_at])
 
+    (a, b) = random.choice((('Alice', 'Bob'), ('Bob', 'Alice')))
+
     if isinstance(event, Prediction):
         return (
-            'Alice turned to Bob and asked, '
-            '"Bob, do you think we will ever %s?"' %
-            render(event.target_event, starting_at, mentioned)
+            '%s turned to %s and asked, '
+            '"%s, do you think we will ever %s?"  '
+            '"I don\'t know, %s," said %s.' % (
+                a, b, b,
+                render(event.target_event, starting_at, mentioned, tense='future'),
+                a, b
+            )
         )
     elif isinstance(event, Memory):
         return (
-            'Bob turned to Alice and said, '
-            '"Alice, do you remember that one time when we %s?"' %
-            render(event.target_event, starting_at, mentioned)
+            '%s turned to %s and said, '
+            '"%s, do you remember that one time when we %s?"  '
+            '%s smiled.  "Of course I do, %s."' % (
+                a, b, b,
+                render(event.target_event, starting_at, mentioned, tense='past'),
+                a, b
+            )
         )
     elif isinstance(event, Sighting):
         return 'Alice and Bob saw a %s.' % event.sight
@@ -53,7 +70,7 @@ def render_toplevel(event):
         raise NotImplementedError(event)
 
 
-def render(event, starting_at, mentioned):
+def render(event, starting_at, mentioned, tense='past'):
     # so much for object-orientation
     if not event:
         return 'nothing at all'
@@ -66,22 +83,42 @@ def render(event, starting_at, mentioned):
 
     mentioned = mentioned | set([event])  # note that we do not modify the shared set
 
+    wonder = 'wondered' if tense == 'past' else 'wonder'
+    remember = 'remembered' if tense == 'past' else 'remember'
+    see = 'saw' if tense == 'past' else 'see'
+
     if isinstance(event, Prediction):
         return (
-            "wonder if we'd ever %s" % render(event.target_event, starting_at, mentioned)
+            "%s if we'd ever %s" % (
+                wonder,
+                render(event.target_event, starting_at, mentioned, tense='future')
+            )
         )
     elif isinstance(event, Memory):
         return (
-            "remember that time when %s" % render(event.target_event, starting_at, mentioned)
+            "%s that time when we %s" % (
+                remember,
+                render(event.target_event, starting_at, mentioned, tense='past')
+            )
         )
     elif isinstance(event, Sighting):
-        return "see a %s" % event.sight
+        return "%s a %s" % (see, event.sight)
     else:
         raise NotImplementedError(event)
 
 
 def main(argv):
-    timeline = [random.choice((Prediction, Memory, Sighting))() for x in xrange(0, 50)]
+    possibilities = (
+        Prediction, Prediction, Memory, Memory, Sighting
+    )
+    timeline = [random.choice(possibilities)() for x in xrange(0, 50)]
+    
+    while isinstance(timeline[0], Memory):
+        timeline = timeline[1:]
+
+    while isinstance(timeline[-1], Prediction):
+        timeline = timeline[:-1]
+
     for (pos, event) in enumerate(timeline):
         event_pool = None
         if isinstance(event, Prediction):
@@ -95,11 +132,18 @@ def main(argv):
             pass
 
     # filter out unlinked events
-    timeline = [t for t in timeline if t.target_event]
+    timeline = [t for t in timeline if isinstance(t, Sighting) or t.target_event]
 
     for event in timeline:
-        print render_toplevel(event)
-        print
+        if isinstance(event, ReferentialEvent):
+            assert event.target_event, event
+
+    for n, event in enumerate(timeline):
+        x = render_toplevel(event)
+        if n > 0:
+            sys.stdout.write('Then one day, ')
+        sys.stdout.write(x)
+        sys.stdout.write('\n\n')
 
 
 if __name__ == '__main__':
