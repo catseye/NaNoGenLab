7bc10544deba61898d801f226001b5bd3d41e6ea
Chris Pressey <cpressey@catseye.tc>
2014-11-17 19:16:36 +0000

    Some progress on advanced-spoonerizer.

--- advanced-spoonerizer/README.md
+++ advanced-spoonerizer/README.md
@@ -15,6 +15,7 @@ Apparatus
 *   Python 2.7.6 (probably works with older versions too)
 *   The `gutenberg.py` module from [gutenizer](https://github.com/okfn/gutenizer/)
 *   A novel from Gutenberg, or other text
+*   `/usr/share/dict/words`
 
 Method
 ------
@@ -22,8 +23,11 @@ Method
 *   Read all the sentences from the text, arranged into sentences.
 *   In each sentence, enumerate all pairings of words.
 *   For each pairing, try swapping the initial consonant clusters.
-    If both resulting words are dictionary words, go ahead with the
-    replacement and output the sentence.
+    Give each pairing a score based on whether one or both are dictionary
+    words (in which case make the score exceedingly high) and the lengths
+    of the words and the number of unique letters involved.
+*   Swap the consonant clusters on the highest-scoring pairing and output
+    the sentence.
 
 Observations
 ------------
--- advanced-spoonerizer/advanced-spoonerizer.py
+++ advanced-spoonerizer/advanced-spoonerizer.py
@@ -24,12 +24,35 @@ def strip_initial_consonants(word):
     return (init, word)
 
 
-def is_dictionary_word(word):
-    return True
+DICTIONARY = {}
+ASCII_LETTERS = set(string.uppercase)
 
 
 def main(argv):
     filenames = argv[1:]
+
+    with open('/usr/share/dict/words', 'r') as f:
+        for line in f:
+            word = line.strip().upper()
+            letters = set(word)
+            if len(word) <= 2 or not letters <= ASCII_LETTERS:
+                continue
+            DICTIONARY[word] = len(letters)
+
+    def clean(word):
+        if word.endswith(('.', '!', '?', ';', ',')):
+            word = word[:-1]
+        if word.startswith(('"', "'", '(')):
+            word = word[1:]
+        if word.endswith(('"', "'", ')')):
+            word = word[:-1]
+        if word.endswith(('.', '!', '?', ';', ',')):
+            word = word[:-1]
+        return word.upper()
+
+    def dictionary_score(word):
+        z = DICTIONARY.get(clean(word), 0)
+        return z
 
     words = []
 
@@ -57,7 +80,8 @@ def main(argv):
     sentences.append(sentence)
 
     for sentence in sentences:
-        #distances = {}  # frozenset of two (word, pos) tuples -> distance
+        scores = {}  # frozenset of two (word, pos) tuples -> score
+        wows = []
         for (pos1, word1) in enumerate(sentence):
             for (pos2, word2) in enumerate(sentence):
                 if word1 == word2:
@@ -68,11 +92,43 @@ def main(argv):
                 new1 = cons2 + base1
                 new2 = cons1 + base2
 
-                if is_dictionary_word(new1) and is_dictionary_word(new2):
-                    x = list(sentence)
-                    x[pos1] = new1
-                    x[pos2] = new2
-                    print ' '.join(x)
+                pair = frozenset([(word1, new1, pos1), (word2, new2, pos2)])
+
+                score1 = dictionary_score(new1) * 100
+                score2 = dictionary_score(new2) * 100
+
+                if score1 > 0 and score2 > 0:
+                    wows.append(pair)
+                    score1 *= 100
+                    score2 *= 100
+
+                score = (
+                    score1 + score2 +
+                    len(new1) + len(new2) + len(set(new1) | set(new2))
+                )
+
+                scores[pair] = score
+
+        wows_only = True
+        if wows_only and not wows:
+            continue
+        if wows_only:
+            print wows
+
+        best_score = -1
+        best_pair = None
+        for pair, score in scores.iteritems():
+            if score > best_score:
+                best_score = score
+                best_pair = pair
+
+        if best_pair is not None:
+            best_pair = list(best_pair)
+            (word1, new1, pos1) = best_pair[0]
+            (word2, new2, pos2) = best_pair[1]
+            sentence[pos2] = new2
+            sentence[pos1] = new1
+            sys.stdout.write(' '.join(sentence) + '  ')
 
 
 if __name__ == '__main__':
