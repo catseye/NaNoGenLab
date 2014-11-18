1c56d1bd0870cfb6c8e5c06285dd0b72af4715a2
Chris Pressey <cpressey@catseye.tc>
2014-11-18 19:52:29 +0000

    Don't crash at the end.

--- shanty-generator/shanty-generator.py
+++ shanty-generator/shanty-generator.py
@@ -10,25 +10,40 @@ DEBUG = False
 
 TEMPLATES = [
     """\
-$^1, $2,
-$^1-$1 $2,
-$^1-$1 $2-$2 $1-$1 $3,
-$^3 $3 $4.
+^$1, $2,
+^$1-$1 $2,
+^$1-$1 $2-$2 $1-$1 $3,
+^$3 $3 $4.
 
 """,
     """\
-$^1, $2-$2,
-$^1, $2-$2,
-$^3-$3 $4!
-$^3-$3 $4!
+^$1, $2-$2,
+^$1, $2-$2,
+^$3-$3 $4!
+^$3-$3 $4!
 """
 ]
 
 
+def get_variables(text):
+    variables = set()
+    while text:
+        if text[0] != '$':
+            text = text[1:]
+        else:
+            text = text[1:]
+            v = '$'
+            while text and text[0].isdigit():
+                v += text[0]
+                text = text[1:]
+            variables.add(v)
+    return list(variables)
+
+
 def fillout(template, words):
     for n in xrange(0, 4):
+        template = template.replace('^$%s' % (n+1), words[n].capitalize())
         template = template.replace('$%s' % (n+1), words[n])
-        template = template.replace('$^%s' % (n+1), words[n].capitalize())
     return template
 
 
@@ -57,8 +72,14 @@ def main(argv):
 
     while words:
         template = random.choice(TEMPLATES)
-        print fillout(template, words)
-        words = words[4:]
+        variables = get_variables(template)
+        use_words = words[:len(variables)]
+        if len(use_words) == len(variables):
+            words = words[len(variables):]
+        else:
+            use_words += ['***'] * (len(variables) - len(use_words))
+            words = []
+        print fillout(template, use_words)
 
 
 if __name__ == '__main__':
