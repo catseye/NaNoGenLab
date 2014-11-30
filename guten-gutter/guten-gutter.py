7ae2f05ce0c04e09e850c2649ce4dded213f5fff
Chris Pressey <cpressey@catseye.tc>
2014-11-30 16:25:03 +0000

    rstrip() lines only!  Begin sketching the MultiCleaner.

--- guten-gutter/guten-gutter.py
+++ guten-gutter/guten-gutter.py
@@ -12,8 +12,16 @@ import sys
 
 
 class SentinelCleaner(object):
-    """Given a file-like object, gives you the lines between the start
-    sentinel (exclusive) and the end sentinel (exclusive.)"""
+    """Given an iterator over lines, gives you the lines between the start
+    sentinel (exclusive) and the end sentinel (exclusive.)
+    
+    The start sentinel is actually "super-exclusive" in that neither it,
+    nor any non-blank lines immediately following it, are included in
+    the output.
+
+    Note that a file-like object is an iterator over lines.  Note that
+    lines produced by this cleaner are stripped of trailing whitespace.
+    """
 
     def __init__(self, fh, start_re=None, end_re=None):
         self.fh = fh
@@ -27,12 +35,12 @@ class SentinelCleaner(object):
 
     def lines(self):
         for line in self.fh:
-            line = line.strip()
+            line = line.rstrip()
             if self.state == 'pre':
                 match = re.match(self.start_re, line.upper())
                 if match:
-                    self.state = 'consuming-produced-by'
-            elif self.state == 'consuming-produced-by':
+                    self.state = 'consuming-start'
+            elif self.state == 'consuming-start':
                 if not line:
                     self.state = 'mid'
             elif self.state == 'mid':
@@ -57,11 +65,40 @@ class ProducedByCleaner(SentinelCleaner):
     END_RE = r'^\**\s*END\s+OF\s+(TH(IS|E)\s+)?PROJECT\s+GUTENBERG.*?$'
 
 
+class MultiCleaner(object):
+    """An object which attempts to apply multiple cleaners to an input.
+    If any cleaner fails, it returns the input just previous to that
+    failure.
+
+    Not fully implemented.  I'm still sketching this...
+    """
+    def __init__(self, strategy=None):
+        if strategy is None:
+            strategy = (GutenbergCleaner, ProducedByCleaner)
+        self.strategy = strategy
+
+    def clean(self, fh):
+        lines = []
+        for line in fh:
+            lines.append(line.rstrip())
+
+        for cleaner_class in self.strategy:
+            cleaner = cleaner_class(lines)
+            new_lines = list(cleaner.lines())
+            if not new_lines:
+                break
+            lines = new_lines
+
+        for line in lines:
+            yield line
+
+
 def main(argv):
     optparser = OptionParser(__doc__.strip())
     optparser.add_option("--output-dir", default=None, metavar='DIR',
-                         help="if given, save result to this directory "
-                              "instead of dumping it to standard output")
+                         help="if given, save the resulting files to this "
+                              "directory (under their original names)"
+                              "instead of dumping them to standard output")
     (options, args) = optparser.parse_args(argv[1:])
 
     for filename in args:
